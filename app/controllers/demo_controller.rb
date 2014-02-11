def230123ceb6b5d9027d6511165f7310f297299
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2014-02-11 03:19:29 -0200

    Adicionado links dinâmicos, Modificado o Demo#hello para usar variaveis do GET e sistema de paginação

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,12 +3,18 @@ class DemoController < ApplicationController
 	layout false
 
   def index
-  	render('demo/hello')
+  	#render('demo/hello')
   end #end of Demo#index
 
   def hello
   	#render('demo/index')
   	@array = [1,2,3,4,5]
+    @id = params[:id]
+    @name = params[:name]
+    @page = params[:page]
+    @algo = params[:algo]
+    puts "Algo: "
+    puts @algo
   end #end of Demo#hello
 
   def other_hello
