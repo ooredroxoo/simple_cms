0487dbf520db06a01247619b78bf35281816b8ca
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2014-02-11 02:43:01 -0200

    add database info and render a hello template in index action of demo controller

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,13 @@
+class DemoController < ApplicationController
+
+	layout false
+
+  def index
+  	render('demo/hello')
+  end #end of Demo#index
+
+  def hello
+  	
+  end #end of Demo#hello
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SimpleCms::Application.routes.draw do
+  #get "demo/index"
+  #match "demo/", :to => "demo#index", :via => :get
+  match ':controller(/:action(/:id))', :via => :get
+  root "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
