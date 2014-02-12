b8d8d67e7370f6acd6ff1e4f75b54d1769397cd3
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2014-02-12 01:44:47 -0200

    Reverted the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212032958) do
-
-  create_table "users", force: true do |t|
-    t.string   "first_name", limit: 25
-    t.string   "last_name",  limit: 50
-    t.string   "email",                 default: "", null: false
-    t.string   "password",   limit: 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140212032508) do
 
 end
