677c74536853c4accb2af2d9d39823b59a550636
Rafael Nascimento Sampaio <ooredroxoo@gmail.com>
2014-02-13 00:38:50 -0200

    User changed to admin_user and subject received some namescopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,23 @@
 class Subject < ActiveRecord::Base
+
+	# obtendo apenas ativos
+	scope :active, lambda {where(:active => true)}
+	# obtendo apenas os inativos
+	scope :not_active, lambda {where(:active => false)}
+	# obtendo apenas visiveis
+	scope :visible, lambda {where(:visible => true)}
+	# obtendo apenas os invisiveis
+	scope :invisible, lambda {where(:visible => false)}
+	# com conteudo
+	scope :with_content_type, lambda {|ctype| where(:with_content_type => ctype)}
+
+	# ordenando
+	scope :sorted, lambda {order("subjects.position ASC")}
+	scope :newest_first, lambda {order("subjects.created_at DESC")}
+
+	# busca 
+	scope :search, lambda { |query| 
+		where(["name LIKE ? ", "%#{query}%"]) 
+	}
+
 end
